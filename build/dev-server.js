a38bcedb6100d47a3136d57499e320d38534d40c
孙佩 <sunpei@MacBook-Air.local>
2017-06-19 22:39:19 +0800

    使用vue-resource 请求数据

--- build/dev-server.js
+++ build/dev-server.js
@@ -21,6 +21,38 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+const appData = require('../data.json');
+const seller = appData.seller;
+const goods = appData.goods;
+const ratings = appData.ratings;
+
+var apiRoutes = express.Router();
+
+apiRoutes.get('/seller',function (req,res) {
+  res.json({
+    errno: 0,
+    data: seller
+  });
+});
+
+apiRoutes.get('/goods',function (req,res) {
+  res.json({
+    errno: 0,
+    data: goods
+  });
+});
+
+apiRoutes.get('/ratings',function (req,res) {
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+});
+
+app.use('/api', apiRoutes)
+
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
