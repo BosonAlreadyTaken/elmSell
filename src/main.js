7039a8abd6bc5ee8512925888a7e00148b5a7410
孙佩 <sunpei@MacBook-Air.local>
2017-06-19 21:51:21 +0800

    1像素边框

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import './common/stylus/index.styl';
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
