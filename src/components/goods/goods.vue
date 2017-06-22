9c54ba3a488b207b13d0da73a093f8eac5bcadca
showa11 <showa11@hotmail.com>
2017-06-22 17:38:47 +0800

    goods组件滚动功能开发完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item, key) in goods" class="menu-item" :class="{'current':currentIndex===key}" @click="selectMenu(key,$event)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
@@ -11,7 +11,7 @@
     </div>
     <div class="foods-wrapper" ref="foodWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -25,7 +25,8 @@
                   <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}</span>
                 </div>
                 <div class="price">
-                  <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                  <span class="now">¥{{food.price}}</span><span class="old"
+                                                                v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -48,25 +49,63 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
     },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      }
+    },
     created() {
+      this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
           this.$nextTick(() => {
             this._initScroll();
+            this._calculateHeight();
           });
         }
       });
-      this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
     },
     methods: {
+      selectMenu(index,event) {
+        if (!event._constructed) {
+          return;
+        }
+        let foodList = this.$refs.foodWrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el,300);
+      },
       _initScroll() {
-        this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
-        this.foodsScroll = new BScroll(this.$refs.foodWrapper, {});
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {click: true});
+        this.foodsScroll = new BScroll(this.$refs.foodWrapper, {
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
       }
     }
   };
@@ -79,7 +118,7 @@
     display: flex
     position: absolute
     top: 174px
-    bottom:46px
+    bottom: 46px
     width: 100%
     overflow: hidden
     .menu-wrapper
@@ -92,6 +131,14 @@
         width: 56px
         padding: 0 12px
         line-height: 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background-color: #fff;
+          font-weight: 700
+          .text
+            border-none()
         .icon
           display: inline-block
           width: 12px
@@ -114,7 +161,7 @@
           display: table-cell
           width: 56px
           vertical-align: middle
-          border-1px(rgba(7,17,27,0.1))
+          border-1px(rgba(7, 17, 27, 0.1))
           font-size: 12px
     .foods-wrapper
       flex: 1
@@ -124,13 +171,13 @@
         line-height: 26px
         border-right: 2px solid #d9dde1
         font-size: 12px
-        color: rgb(147,153,159)
+        color: rgb(147, 153, 159)
         background: #f3f5f7
       .food-item
         display: flex
         margin: 18px
         padding-bottom: 18px
-        border-1px(rgba(7,17,27,0.1))
+        border-1px(rgba(7, 17, 27, 0.1))
         &:last-child
           border-none()
           margin-bottom: 0
@@ -144,11 +191,11 @@
             height: 14px
             line-height: 14px
             font-size: 14px
-            color: rgb(7,17,27)
-          .desc ,.extra
+            color: rgb(7, 17, 27)
+          .desc, .extra
             line-height: 10px
             font-size: 10px
-            color: rgb(147,153,159)
+            color: rgb(147, 153, 159)
           .desc
             margin-bottom: 8px
             line-height: 12px
@@ -161,10 +208,10 @@
             .now
               margin-right: 8px
               font-size: 14px
-              color: rgb(240,20,20)
+              color: rgb(240, 20, 20)
             .old
               text-decoration: line-through
               font-size: 10px
-              color: rgb(147,153,159)
+              color: rgb(147, 153, 159)
 
 </style>
